b90e4b72e69e8523f7a826d25984cd7553e6366d
Maciej Mrowiec <mrowiec.maciej@gmail.com>
2015-11-23 14:41:21 +0100

    Add application.

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import cli "github.com/maciejmrowiec/go_travis_test/Godeps/_workspace/src/github.com/ivpusic/go-clicolor/clicolor"
+
+func PrintMeSomething() {
+	cli.Print("some text").In("green")
+	cli.Print("{red}Some text in red. {white}Some text in white. {default}Some text in default color").InFormat()
+}
+
+func main() {
+	PrintMeSomething()
+}
